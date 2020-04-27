8e96cb52852dd8054166c59cb2f0ce066e34f035
liamSully43 <60324016+liamSully43@users.noreply.github.com>
2020-04-27 16:02:44 +0100

    Initial Push of the Bootstrap Documentation Recreation
    Initial Push

---
+++ bootstrap.js
@@ -0,0 +1,12 @@
+//popovers
+$(function(){
+    $('[data-toggle=\'popover\']').popover()
+});
+
+//toasts
+$('.toast').toast();
+
+//tooltips
+$(function(){
+    $('[data-toggle=\'tooltip\']').tooltip();
+});
